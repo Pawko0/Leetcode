6b28d81e2b2b955ae823c0e9d42d76e0bae8dfc3
Paweł <pawelmitko1@gmail.com>
2023-12-23 13:27:15 +0100

    commit#2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         String[] words = {"hey", "aeo", "artro", "mu", "ooo"};
-        System.out.println(vowelStrings(words, 1, 4));
+        System.out.println(VowelString.vowelStrings(words, 1, 4));
     }
 
-    public static int vowelStrings(String[] words, int left, int right) {
-        String vowel = "aeiou";
-
-        int count = 0;
-
-        for (int i = left; i <= right; i++) {
-            if (vowel.contains(String.valueOf(words[i].charAt(0))) && vowel.contains(String.valueOf(words[i].charAt(words[i].length() - 1))))
-                count++;
-        }
-
-        return count;
-    }
 
 }
